b5180794aceba42d45f7ace32fb70001700b2576
Matthew Marino <mmarino@jcmar.com>
2014-08-06 09:14:44 -0700

    moved to sails-generate-frontend-gulp

--- templates/tasks/config/clean.js
+++
@@ -1,7 +0,0 @@
- module.exports = function(gulp, plugins) {
-	gulp.task('clean', function() {
-		return gulp.src(['.tmp/public/styles', '.tmp/public/js', '.tmp/public/images'], {read: false})
-				.pipe(plugins.rimraf())
-				.pipe(plugins.notify({ message: 'Clean task complete' }));;
-	});
-};
