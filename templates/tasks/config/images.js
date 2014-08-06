5793d6ea869d9afc753df538e32a9fdfb43b38b2
Matthew Marino <mmarino@jcmar.com>
2014-08-06 09:26:54 -0700

    moved to sails-generate-frontend-gulp

--- templates/tasks/config/images.js
+++
@@ -1,8 +0,0 @@
- module.exports = function(gulp, plugins) {
-	 gulp.task('images', function() {
-		return gulp.src('assets/images/**/*')
-				.pipe(plugins.cache(plugins.imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-				.pipe(gulp.dest('.tmp/public/images'))
-				.pipe(plugins.notify({ message: 'Images task complete' }));
-	});
-};
